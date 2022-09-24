334f53ec501898eb3cd2d564e2688446d2c5eb42
oteca58 <oteca@protonmail.com>
2022-09-24 13:29:54 +0200

    added mongoDB and mongoose connections to server

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 const app = require("./app");
 
 dotenv.config({ path: "./config/config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("DB connection successful!");
+  });
 
 const port = process.env.PORT || 3000;
 
