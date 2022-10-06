175797608eb1f34c31d160b8008bb9ebc7e32b85
Mrleon123 <Mrleon@hotmail.it>
2022-10-06 09:26:44 +0200

    server & synchronous error implementation

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = require("./app");
 
 dotenv.config({ path: "./config/config.env" });
 
+//handling of synchronous errors
+process.on('uncaugthExeption', err=>{
+  console.log(err.name, err.message)
+  console.log('UNCAUGTH EXEPTION!  Shutting down...')
+  process.exit(1);
+});
+
 //replace password with
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -20,8 +27,15 @@ mongoose
   
   const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+//server connection error handling
+process.on('unhandledRejection', err=>{
+  console.log(err.name, err.message)
+  console.log('UNHANDLED REJECTION!  Shutting down...')
+  server.close(()=>{  //with the close it ends all pending promises
+    process.exit(1);
+  });
 
-
+});
