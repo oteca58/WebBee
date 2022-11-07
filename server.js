af0037fe87e0ff1e415df1674010165ef7cedb63
oteca58 <oteca@protonmail.com>
2022-11-07 19:11:05 +0100

    bug/testing app unsuccessful

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 const app = require("./app");
 
+const { MongoMemoryServer } = require("mongodb-memory-server");
+let mongod = null;
+
 //manage uncaught exception
 process.on("uncaughtException", (err) => {
   console.log("Uncaught Exception. Shutting down...");
@@ -11,33 +14,59 @@ process.on("uncaughtException", (err) => {
 
 dotenv.config({ path: "./config/config.env" });
 
-//replace password with
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
+const connectDB = async () => {
+  try {
+    //replace password with
+    let DB = process.env.DATABASE.replace(
+      "<PASSWORD>",
+      process.env.DATABASE_PASSWORD
+    );
+    if (process.env.NODE_ENV === "test") {
+      mongod = await MongoMemoryServer.create();
+      DB = mongod.getUri();
+    }
 
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//   })
-//   .then(() => {
-//     console.log("DB connection successful!");
-//   });
-//.catch(err => console.log("ERROR")); if password is wrong print error
+    const conn = await mongoose
+      .connect(DB, {
+        useNewUrlParser: true,
+      })
+      .then(() => {
+        console.log("DB connection successful!");
+      });
+  } catch {
+    (err) => console.log("ERROR"); //if password is wrong print error
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.connection.close();
+    if (mongod) {
+      await mongod.stop();
+    }
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 const port = process.env.PORT || 3000;
 
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
-
-//manage other errors
-process.on("unhandledRejection", (err) => {
-  console.log("Unhandled Rejection. Shutting down...");
-  console.log(err.name, err.message);
-  //before closing server and after the app
-  server.close(() => {
-    process.exit(1); //0 is for success and 1 is for errors
+function createHttpserver(port) {
+  const app = require("./app");
+  const server = app.listen(port, () => {
+    console.log(`App running on port ${port}`);
   });
-});
+  return server;
+}
+//manage other errors DA RIMETTERE
+// process.on("unhandledRejection", (err) => {
+//   console.log("Unhandled Rejection. Shutting down...");
+//   console.log(err.name, err.message);
+//   //before closing server and after the app
+//   server.close(() => {
+//     process.exit(1); //0 is for success and 1 is for errors
+//   });
+// });
+//if (process.env.NODE_ENV !== "test") createHttpserver(port);
+module.exports = { connectDB, disconnectDB, createHttpserver };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,9 @@
-const supertest = require("supertest");
-const app = require("./../app");
-const { server } = require("./../server");
-const request = supertest(app);
-
-const { connectDB, disconnectDB } = require("./mockDB");
+//const supertest = require("supertest");
+const { agent } = require("supertest");
+//const app = require("./../app");
+const { createHttpserver, connectDB, disconnectDB } = require("./../server");
+const httpServer = createHttpserver(3000);
+const request = agent(httpServer);
 
 describe("API test", () => {
   beforeAll(() => {
@@ -12,10 +12,13 @@ describe("API test", () => {
 
   afterAll(() => {
     disconnectDB();
-    server.close();
+    httpServer.close();
   });
+
   describe("POST /api/v1/users", () => {
     it("example request using a mocked database instance", async () => {
+      let url = request.get("/api/v1/users/signup");
+      console.log(url.url);
       const res = await request.post("/api/v1/users/signup", {
         name: "beeadopter",
         email: "beeadopter@webBee.it",
@@ -23,7 +26,7 @@ describe("API test", () => {
         passwordConfirm: "beeadopter",
         role: "beeadopter",
       });
-      jest.setTimeout(10000);
+      //jest.setTimeout(10000);
       expect(res.status).toBe(201);
     });
   });
